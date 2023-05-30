c8946d763e721c18090653ba3ff7ae8ed6a2e39c
Weston Schutt <wtschutt@gmail.com>
2023-05-29 19:07:23 -0600

    Add iteration 2 with Library class tests and methods

---
+++ lib/library.rb
@@ -0,0 +1,16 @@
+class Library
+  attr_reader :name, :books, :authors
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
+  end
+end
---
+++ spec/library_spec.rb
@@ -0,0 +1,46 @@
+require './lib/library'
+require './lib/author'
+require './lib/book'
+
+RSpec.describe Library do
+  before(:each) do
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  it 'exists' do
+    expect(@dpl).to be_a(Library)
+  end
+
+  it 'has a name' do
+    expect(@dpl.name).to eq("Denver Public Library")
+  end
+
+  it 'starts with no books' do
+    expect(@dpl.books).to eq([])
+  end
+
+  it 'starts with no authors' do
+    expect(@dpl.authors).to eq([])
+  end
+
+  it 'can add authors' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    expect(@dpl.authors).to eq([charlotte_bronte, harper_lee])
+  end
+
+  it 'automatically adds author books to collection' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847") 
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    @dpl.add_author(charlotte_bronte)
+
+    expect(@dpl.books).to eq([jane_eyre, professor, villette])
+  end
+end
