009fd795d45f0175fd9ee09bda8952854a01c26d
Weston Schutt <wtschutt@gmail.com>
2023-05-29 18:36:21 -0600

    Add iteration 1 tests and class with methods

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+require 'date'
+
 class Book
+  attr_reader :info
+
+  def initialize(info)
+    @info = info
+  end
+
+  def title
+    info[:title]
+  end
+
+  def author
+   "#{info[:author_first_name]} #{info[:author_last_name]}"
+  end
+
+  def publication_year
+    string = info[:publication_date]
+    Date.parse(string).year
+  end
+
+
 end 
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,2 +1,23 @@
-require 'rspec'
+require './lib/book'
 
+RSpec.describe Book do
+  before(:each) do
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  it 'exists' do
+    expect(@book).to be_a(Book)
+  end
+
+  it 'has a title' do
+    expect(@book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it 'has an author' do
+    expect(@book.author).to eq("Harper Lee")
+  end
+
+  it 'has a publication year' do
+    expect(@book.publication_year).to eq(1960)
+  end
+end
